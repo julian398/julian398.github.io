a4552d8b636ba3ea6381ae0da589c87d6fe8b4fb
unknown <julian398a@yahoo.es>
2023-05-01 22:08:35 -0500

    Updates

---
+++ precache-manifest.a96f064330294653a97aec18a3703145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a019539b0edafaa147e",
+    "url": "/julian398.github.io/static/js/main.4a019539.chunk.js"
+  },
+  {
+    "revision": "decdc75123c9397935ed",
+    "url": "/julian398.github.io/static/js/1.decdc751.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/julian398.github.io/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "afb1199f3e313e5f2271",
+    "url": "/julian398.github.io/static/js/runtime~main.afb1199f.js"
+  },
+  {
+    "revision": "45b43a3a1eaec824e0ef46cd97065cd9",
+    "url": "/julian398.github.io/index.html"
+  }
+];
